aa579e6621e429185509b7e2bc43b1f7e0188ec2
Nef <maximenef1@gmail.com>
2022-09-27 14:35:40 +0200

    update page bien vendu

--- src/pages/vendu/index.jsx
+++ src/pages/vendu/index.jsx
@@ -5,10 +5,26 @@ import Container from "../../components/shared/container";
 import CardCTA from "../../components/shared/card-Cta";
 import CardBienVendre from "../../components/biens/cardBienVendre";
 import CardBienVendu from "../../components/biens/cardBiensVendu";
+import MotionRight from "../../components/shared/motion-CardRight";
 
 const Avendre = ({ pages, locations }) => {
   return (
     <NavPage current='Avendre'>
+      <H1>{"Nos Biens vendu"}</H1>
+      <MotionRight
+        initial='hidden'
+        animate='visible'
+        transition={{ duration: 0.7, delay: 0.9 }}
+      >
+        <CardCTA
+          text1={"Ne manquez plus nos nouveaux biens !"}
+          text2={
+            "Quand vous trouvez enfin un bien qui correspond à vos attentes, on vous annonce qu’il est déjà vendu... En vous inscrivant cela ne vous arrivera plus !"
+          }
+          text3={"Nos biens >"}
+          src='/pages'
+        />
+      </MotionRight>
       <CardBienVendu pages={pages} locations={locations} />
       <Container className='relative z-20 h-44 w-full'></Container>
     </NavPage>
