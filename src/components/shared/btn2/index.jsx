70e2ba94608bed38f84011ac6a3e8c440dbaab9c
Nef <maximenef1@gmail.com>
2023-01-17 15:59:39 +0100

    remove switch case btn2

--- src/components/shared/btn2/index.jsx
+++ src/components/shared/btn2/index.jsx
@@ -1,23 +1,6 @@
 import Link from "next/link";
 
 export default function Btn2(props) {
-  function findBgClass() {
-    switch (props.color) {
-      case "white":
-        return "  bg-white px-4 py-2 ";
-      case "blueu":
-        return "bg-gradient-to-r from-[#41B8B8] to-[#1AD9D9] px-2 py-2 ";
-    }
-  }
-  function findtextClass() {
-    switch (props.color) {
-      case "white":
-        return " text-[19px]  text-transparent bg-clip-text bg-gradient-to-r from-[#41B8B8] to-[#1AD9D9] font-semibold leading-4";
-      case "blueu":
-        return " text-white text-[11px] font-bold leading-[14px]";
-    }
-  }
-
   return (
     <div className='w-full flex flex-col justify-center '>
       {" "}
@@ -28,7 +11,11 @@ export default function Btn2(props) {
             id={props.id}
             className={` w-fit ${
               props.className
-            } justify-center  items-center ${findBgClass()} rounded-[40px] `}
+            } justify-center  items-center ${
+              props.color == "white"
+                ? " bg-white px-4 py-2"
+                : "bg-gradient-to-r from-[#41B8B8] to-[#1AD9D9] px-2 py-2"
+            } rounded-[40px] `}
             style={{
               ...(props.fitHeight && { height: "fit-content" }),
               ...props.style,
@@ -36,7 +23,15 @@ export default function Btn2(props) {
             onMouseEnter={props.onMouseEnter}
             onMouseLeave={props.onMouseLeave}
           >
-            <p className={`  ${findtextClass()} `}>{props.text}</p>{" "}
+            <p
+              className={`  ${
+                props.color == "white"
+                  ? "text-[19px]  text-transparent bg-clip-text bg-gradient-to-r from-[#41B8B8] to-[#1AD9D9] font-semibold leading-4"
+                  : "text-white text-[11px] font-bold leading-[14px]"
+              } `}
+            >
+              {props.text}
+            </p>{" "}
           </div>{" "}
         </a>
       </Link>
