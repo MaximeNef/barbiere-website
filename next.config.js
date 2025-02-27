1ffb5ebb58d1dbb9f67ca32c4c26bb3283362c35
Nef Maxime <maximenef1@gmail.com>
2025-02-27 16:10:03 +0100

    change swc to false

--- next.config.js
+++ next.config.js
@@ -4,14 +4,13 @@ const nextConfig = {
   env: {
     SG_API_KEY: process.env.NEXT_PUBLIC_BARBIERE_MAIL,
   },
-  swcMinify: true,
+  swcMinify: false,
   images: {
     // minimumCacheTTL: 90,
     // loader: "imgix",
     path: "",
     domains: ["images.prismic.io"],
   },
-  swcMinify: true, // Assure-toi d'utiliser SWC sans WebAssembly
 };
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
